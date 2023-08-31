2cbd8304001c08a360734046e13a216df9885074
sulaidat <tuyenvx.lqd@gmail.com>
2023-08-31 16:11:54 +0700

    update

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 git.exe add . && echo "[1] added" \
 && git.exe commit -m "update" && echo "[2] commited" \
-&& eval $(ssh-agent -s) && ssh-add ~/.ssh/id_slaydark && echo "[3] add key" \
+&& eval $(ssh-agent -s) && ssh-add ~/.ssh/id_slaydark && echo "[3] added key" \
 && /usr/bin/git push && echo "[4] pushed"
